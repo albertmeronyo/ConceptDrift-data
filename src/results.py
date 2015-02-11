fef757a83130e56dcdc4d31ec327e7be656e1de2
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2015-02-11 17:45:00 +0100

    Converting results to CSV done

--- src/results.py
+++ src/results.py
@@ -11,16 +11,20 @@ data = inFile.readlines()
 results = []
 
 for i,line in enumerate(data):
-    if line.split()[0] == 'java':
+    if data[i].split()[0] == 'java':
+        exp = sys.argv[1]
+        mode = "TRAINING"
+        if not line.split()[4].split('.')[2] == "meta":
+            mode = "TEST"
         # Look if next lines contain results
-        if line[i+1].split()[0] == "Weighted":
-            results.append([line + line[i+1].split()[2:9]])
-        if line[i+2].split()[0] == "Weighted":
-            results.append([line + line[i+2].split()[2:9]])
+        if i + 1 < len(data):
+            if data[i+1].split()[0] == "Weighted":
+                results.append([exp] + [line] + [mode] + data[i+1].split()[2:9])
+        if i + 2 < len(data):
+            if data[i+2].split()[0] == "Weighted":
+                results.append([exp] + [line] + ["10CV"] + data[i+2].split()[2:9])
 
-with open('results.csv') as outFile:
+with open('results.csv', 'wb') as outFile:
     writer = csv.writer(outFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     for line in results:
         writer.writerow(line)
-
-    
